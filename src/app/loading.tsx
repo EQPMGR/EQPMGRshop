d8aa8baeba82043bf2f0f4312808a31ac196a622
Sage Davies <sagelovestheforest@gmail.com>
2025-08-17 04:52:40 +0000

    On the load screen, can we omit "Central" and have it say EQPMGR Shop?

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,4 @@
+
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
@@ -10,7 +11,7 @@ export default function Loading() {
             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
           </svg>
         </div>
-        <p className="text-muted-foreground font-headline">Loading EQPMGRshop Central...</p>
+        <p className="text-muted-foreground font-headline">Loading EQPMGR Shop...</p>
       </div>
     </div>
   );
