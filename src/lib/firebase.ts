b20b5335a5559432a217d413510c2e0b671fcea5
Sage Davies <sagelovestheforest@gmail.com>
2025-10-20 03:25:06 +0000

    I don't think that will work. The firestore db is in eqpmgr-test, not eq

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,8 +8,8 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "eqpmgr-test.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  projectId: "eqpmgr-test",
+  storageBucket: "eqpmgr-test.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
